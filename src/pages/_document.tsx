df99aa6d266620cfe645c7b26f8107d255273351
Tassio-Med <tassiohfmed@gmail.como>
2023-03-10 16:47:54 -0300

    :tada: first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+      </Head>
       <body>
         <Main />
         <NextScript />

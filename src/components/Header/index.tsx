240182aaf5e7bcd7b11cd4756a582abcc739a933
Tassio-Med <tassiohfmed@gmail.como>
2023-03-13 11:59:31 -0300

    :sparkles: feat: componente Header criado e estilizado.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/imagens/logo.png'
+
+import ActiveLink from '../ActiveLink';
+
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <ActiveLink href="/" activeClassName={styles.active}>
+          <a>
+            <Image src={logo} alt="Estrela da Morte logo"/>
+          </a>
+        </ActiveLink>
+
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Conteúdos</a>
+          </ActiveLink>
+          <ActiveLink href="/sobre" activeClassName={styles.active}>
+            <a>Quem somos</a>
+          </ActiveLink>
+        </nav>
+
+        <a className={styles.readyButton} type="button" href="https://sujeitoprogramador.com">COMEÇAR</a>
+
+      </div>
+    </header>
+  )
+}

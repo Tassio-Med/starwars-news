85894d2eab138d7c9201ded13f2b3b74cba9829d
Tassio-Med <tassiohfmed@gmail.como>
2023-03-14 18:14:32 -0300

    :sparkles: feat: estrutura da  página de posts(por slug) criada.

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,10 @@
 import { GetServerSideProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom';
-import styles from './post.module.scss';
 
+import styles from './post.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
 
 interface PostProps {
   post: {
@@ -16,9 +18,28 @@ interface PostProps {
 
 export default function Post({ post }: PostProps ) {
   return (
-    <div>
-      <h1>Detalhes do post</h1>
-    </div>
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head> 
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <Image
+            quality={100}
+            src={post.cover}
+            width={720}
+            height={410}
+            alt={post.title}
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+          />
+
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: post.description }}></div>
+        </article>
+      </main>
+    </>
   )
 }
 

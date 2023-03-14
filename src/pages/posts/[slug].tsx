92ee19b814c3787034169c06417770c798b74491
Tassio-Med <tassiohfmed@gmail.como>
2023-03-14 18:11:17 -0300

    :sparkles: feat: página de posts(por slug) puxando todas a informações do CMS

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,57 @@
+import { GetServerSideProps } from 'next';
+import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
+import styles from './post.module.scss';
+
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    description: string;
+    cover: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps ) {
+  return (
+    <div>
+      <h1>Detalhes do post</h1>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async({ req, params }) => {
+  const { slug } =  params;
+  const prismic = getPrismicClient(req);
+
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  if(!response){
+    return {
+      redirect:{
+        destination: '/posts',
+        permanent: false
+      }
+    }
+  }
+
+  const post = {
+    slug: slug,
+    title: RichText.asText(response.data.title),
+    description: RichText.asHtml(response.data.description),
+    cover: response.data.cover.url,
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  };
+
+  return {
+    props: {
+      post
+    }
+  }
+}

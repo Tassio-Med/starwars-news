a95c990d858ccb7363bc553d063ef5f424f8170a
Tassio-Med <tassiohfmed@gmail.como>
2023-03-14 16:10:05 -0300

    :sparkles: feat: organizand na página post os elementos que são puxados do CMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,55 +1,64 @@
-import { GetStaticProps } from "next";
-import { getPrismicClient } from "../../services/prismic";
-import { RichText } from "prismic-dom";
-import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
-import Head from "next/head"
+import Head from 'next/head';
 
 import styles from './styles.module.scss';
-import Link from "next/link";
-import Image from "next/image";
-import thumbImg from '../../../public/imagens/chicagobanner.jpg'
+import Link from 'next/link';
+
+import Image from 'next/image';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText} from 'prismic-dom'; 
+
+// https://png-pixel.com/
 
 import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
 
 type Post = {
   slug: string;
-  title: string; 
-  description: string; 
-  cover: string; 
+  title: string;
+  cover: string;
+  description: string;
   updatedAt: string
 }
 
-interface PostsProps {
+interface PostsProps{
   posts: Post[];
 }
 
-export default function Posts({ posts }: PostsProps) {
-  return (
+export default function Posts({ posts: postsBlog }: PostsProps){
+
+  const [posts, setPosts] = useState(postsBlog || []);
+
+  return(
     <>
-      <Head>
-        <title>
-          Blog | Star Wars News
-        </title>
-      </Head>
-      <main className={styles.container}>
-        <div className={styles.posts}>
-          <Link href="/" legacyBehavior>
-            <a>
+     <Head>
+       <title>Blog | Star Wars News</title>
+     </Head>
+     <main className={styles.container}>
+       <div className={styles.posts}>
+        {posts.map( post => (
+          <Link key={post.slug} href={`/posts/${post.slug}`} legacyBehavior>
+            <a key={post.slug}>
               <Image 
-                src={thumbImg}
-                alt="Banner do evento em Chicago"
+                src={post.cover} 
+                alt={post.title}
                 width={720}
                 height={410}
                 quality={100}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
               />
-              <strong>Criando meu primeiro aplicativo</strong>
-              <time>14 JULHO 2021</time>
-              <p>Hoje vamos criar o controle de mostrar a senha no input, uma opção para os nossos formulários de cadastro e login. Mas chega de conversa e bora pro código junto comigo que o vídeo está show de bola!</p>
+              <strong>{post.title}</strong>
+              <time>{post.updatedAt}</time>
+              <p>{post.description}</p>
             </a>
           </Link>
+        ))}
 
-          <div className={styles.buttonNavigate}>
+         <div className={styles.buttonNavigate}>
             <div>
               <button>
                 <FiChevronsLeft size={25} color="#FFF" />
@@ -69,19 +78,20 @@ export default function Posts({ posts }: PostsProps) {
             </div>
             
          </div>
-        </div>
-      </main>
+       </div>
+     </main>
     </>
   )
 }
+
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
   const response = await prismic.query([
-    Prismic.Predicates.at('documente.type', 'post')
+    Prismic.Predicates.at('document.type', 'post')
   ], {
-    orderings: '[document.last_publication_date desc]',
+    orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
     fetch: ['post.title', 'post.description', 'post.cover'],
     pageSize: 3
   })
@@ -92,7 +102,7 @@ export const getStaticProps: GetStaticProps = async () => {
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
-      description: post.data.descritption.find(content => content.type === 'paragraph')?.text ?? '',
+      description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
       cover: post.data.cover.url,
       updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
         day: '2-digit',
@@ -102,11 +112,10 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
-  return {
-    props: {
+  return{
+    props:{
       posts
     },
-    revalidate: 60 * 30
+    revalidate: 60 * 30 // Atualiza a cada 30 minutos.
   }
 }
-
